b01f61453638fd41b37b2ad3cd60eb16a4d70ad2
Nicollas R <nicollasfernandesricas@gmail.com>
2021-08-25 08:15:22 -0400

    Update stream deck distribution tool Refactored postbuild script

--- postbuild.ps1
+++ postbuild.ps1
@@ -1,22 +1,24 @@
-param($params)
+param([string]$targetName, [string]$outputDir)
 
-$arguments = $params.replace("`"", "").split(',') # MTF double quote thing
+Write-Host $targetName
+Write-Host $outputDir
 
-$targetName = $arguments[0]
-$targetDir = $arguments[1]
+$pluginPath = "$Env:APPDATA\Elgato\StreamDeck\Plugins\$targetName.sdPlugin"
+
+Write-Host $pluginPath
 
 taskkill /f /im "StreamDeck.exe"
 
 taskkill /f /im "$targetName.exe"
 
-$pluginPath = "$env:APPDATA\Elgato\StreamDeck\Plugins\$targetName.sdPlugin"
-
 Remove-Item -Path "$pluginPath" -Force -Recurse
 
-Copy-Item -Path "$targetDir" -Destination "$pluginPath" -Force -Recurse -Container: $false -Exclude "*.pdb"
+Copy-Item -Path "$outputDir" -Destination "$pluginPath" -Force -Recurse -Container: $false -Exclude "*.pdb"
 
 Start-Process -FilePath "C:\Program Files\Elgato\StreamDeck\StreamDeck.exe"
 
-Remove-Item "$PSScriptRoot\$targetName.streamDeckPlugin"
+Remove-Item "$PSScriptRoot\$targetName.streamDeckPlugin" -Force -Recurse
 
-Start-Process -FilePath "$PSScriptRoot\tools\DistributionTool.exe" -ArgumentList "-b -i `"$pluginPath`" -o `"$PSScriptRoot`"" -NoNewWindow
+Write-Host "$($outputDir)$targetName.sdPlugin"
+
+. "$PSScriptRoot\tools\DistributionTool.exe" -b -i "$pluginPath" -o "$PSScriptRoot" -NoNewWindow
